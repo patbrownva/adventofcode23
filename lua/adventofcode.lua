89ead4a365e9a9714484e836150c6ed8cd174a12
Pat Brown <patbrownwork@outlook.com>
2023-12-01 13:01:43 -0500

    Day 1: Lua version part 2

--- lua/adventofcode.lua
+++ lua/adventofcode.lua
@@ -4,7 +4,7 @@ M.sum = function (a, b)
     return (a or 0) + (b or 0)
 end
 
-M.reduce = function (F, T)
+M.reduce = function (T, F)
     local R
     for _,v in pairs(T) do
         R = F(R, v)
@@ -12,11 +12,19 @@ M.reduce = function (F, T)
     return R
 end
 
-M.map = function (F, T)
+M.map = function (T, F)
     local R = {}
     for k,v in pairs(T) do
         R[k] = F[v]
     end
+    return R
+end
+
+M.index = function (T, V)
+    for k,v in pairs(T) do
+        if v == V then return k end
+    end
+    return nil
 end
 
 return M
--- lua/trebuchet.lua
+++ lua/trebuchet.lua
@@ -1,8 +1,55 @@
 local aoc = require"adventofcode"
 
+local function find_first_of(str, pats)
+    local found_pos = #str + 1
+    local found
+    for _, pat in pairs(pats) do
+        local pos, pose = string.find(str, pat)
+        if pos and pos < found_pos then
+            found = string.sub(str, pos, pose)
+            found_pos = pos
+        end
+        -- small optimization if we have the first possible match
+        if pos == 1 then
+            break
+        end
+    end
+    return found
+end
+
+local function find_last_of(str, pats)
+    for pos = -1, -#str, -1 do
+        for _, pat in pairs(pats) do
+            local found = string.match(str, pat, pos)
+            if found then return found end
+        end
+    end
+    return nil
+end
+
 function trebuchet1 (line)
+    line = string.lower(line)
     local first = string.match(line, '%d') or ''
     local last = string.match(string.reverse(line), '%d') or ''
+    return tonumber(first .. last) or 0
+end
+
+local number_words = {
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero', '%d'
+}
+local function word_to_number (word)
+    local num = tonumber(word)
+    if num then return num end
+    if word == 'zero' then return 0 end
+    return aoc.index(number_words, word)
+end
+
+function trebuchet2 (line)
+    line = string.lower(line)
+    local first = find_first_of(line, number_words)
+    local last = find_last_of(line, number_words)
+    first = first and word_to_number(first) or ''
+    last = last and word_to_number(last) or ''
     return tonumber(first .. last) or 0
 end
 
@@ -12,4 +59,4 @@ local lines = {}
 for line in io.lines() do
     lines[#lines+1] = stage(line)
 end
-print(aoc.reduce(aoc.sum, lines))
+print(aoc.reduce(lines, aoc.sum))
