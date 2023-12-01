571eee57eaaa7c6660379d901a31d14b83254907
Pat Brown <patbrownwork@outlook.com>
2023-12-01 01:16:59 -0500

    Day 1 in Python

---
+++ python/trebuchet.py
@@ -0,0 +1,46 @@
+# Advent of Code 2023
+# Day one task: Trepuchet?!
+
+import sys
+import re
+
+NUM = re.compile('\\d')
+number_words = {
+    'zero':0,
+    'one':1,
+    'two':2,
+    'three':3,
+    'four':4,
+    'five':5,
+    'six':6,
+    'seven':7,
+    'eight':8,
+    'nine':9
+}
+NUM_WORDS = re.compile('|'.join(number_words)+'|\\d')
+def word_to_int(word):
+    return int(number_words.get(word,word))
+
+def safe_search(match):
+    return match and match.group(0) or ''
+
+def find_all_overlapping(exp,line):
+    start = 0
+    while match := exp.search(line, start):
+        yield match.group(0)
+        start = match.start() + 1
+
+def trebuchet1(line):
+    return int(safe_search(NUM.search(line)) + safe_search(NUM.search(line[::-1])))
+
+def trebuchet2(line):
+    # My first attempt here used `findall` but failed on 'eightwo'
+    numbers = list(find_all_overlapping(NUM_WORDS, line))
+    return numbers and (word_to_int(numbers[0])*10 + word_to_int(numbers[-1])) or 0
+    
+def main():
+    import sys
+    print(sum(map(trebuchet2, sys.stdin)))
+
+if __name__=='__main__':
+    main()
