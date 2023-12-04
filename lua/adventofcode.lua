960068b3496cf3503e2636f7425d101ef6bc67bb
Pat Brown <patbrownwork@outlook.com>
2023-12-04 01:56:05 -0500

    Put some useful functions in the shared module

--- lua/adventofcode.lua
+++ lua/adventofcode.lua
@@ -15,7 +15,7 @@ end
 M.map = function (T, F)
     local R = {}
     for k,v in pairs(T) do
-        R[k] = F[v]
+        R[k] = F(v)
     end
     return R
 end
@@ -25,6 +25,58 @@ M.index = function (T, V)
         if v == V then return k end
     end
     return nil
+end
+
+M.findall = function (S, pat)
+    local R = {}
+    for M in string.gmatch(S, "%d+") do
+        R[#R+1] = M
+    end
+    return R
+end
+
+M.intersect = function (A, B)
+    local S = {}
+    for _, I in ipairs(A) do
+        S[I] = true
+    end
+    local R = {}
+    for _, I in ipairs(B) do
+        R[#R+1] = S[I] and I
+    end
+    return R
+end
+
+M.union = function (A, B)
+    if #B > #A then
+        A,B = B,A
+    end
+    local S = {}
+    local R = {}
+    for _,I in ipairs(A) do
+        S[I] = true
+        R[#R+1] = I
+    end
+    for _,I in ipairs(B) do
+        if not S[I] then
+            R[#R+1] = I
+        end
+    end
+    return R
+end
+
+M.difference = function (A, B)
+    local S = {}
+    local R = {}
+    for _,I in ipairs(B) do
+        S[I] = true
+    end
+    for _,I in ipairs(A) do
+        if not S[I] then
+            R[#R+1] = I
+        end
+    end
+    return R
 end
 
 M.run_reduce_sum = function (stages, ...)
--- lua/scratchcards.lua
+++ lua/scratchcards.lua
@@ -1,23 +1,7 @@
 local aoc = require"adventofcode"
     
 function list_of_nums(line)
-    local list = {}
-    for match in line:gmatch("%d+") do
-        list[#list+1] = tonumber(match)
-    end
-    return list
-end
-
-function intersect(left, right)
-    local source = {}
-    for _, item in ipairs(left) do
-        source[item] = 1
-    end
-    local collect = {}
-    for _, item in ipairs(right) do
-        collect[#collect+1] = source[item] and item
-    end
-    return collect
+    return aoc.map(aoc.findall(line, "%d+"), tonumber)
 end
 
 function card(line)
@@ -29,14 +13,14 @@ local CARDS = {}
 
 function scratchcards1(line)
     local card_num, winning, drawn = card(line)
-    local matches = intersect(winning, drawn)
+    local matches = aoc.intersect(winning, drawn)
     return #matches > 0 and math.tointeger(2^(#matches-1)) or 0
 end
 
 function scratchcards2(line)
     local card_num, winning, drawn = card(line)
     CARDS[card_num] = (CARDS[card_num] or 0) + 1
-    local matches = intersect(winning, drawn)
+    local matches = aoc.intersect(winning, drawn)
     if #matches > 0 then
         for win_card = card_num+1, card_num+#matches do
             CARDS[win_card] = (CARDS[win_card] or 0) + CARDS[card_num]
